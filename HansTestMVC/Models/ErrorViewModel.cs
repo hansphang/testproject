fbc4a1acaaaaad5e1788365742b51f4c354c49c8
hansphang <hans.phang@gmail.com>
2018-03-04 14:57:04 -0800

    Initial check-in of module HansTestMVC

---
+++ HansTestMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HansTestMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
